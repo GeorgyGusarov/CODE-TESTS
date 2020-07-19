be3b8cf6a6fed09d601dad2924dada20c478b13e
Georgy <gogalsh@gmail.com>
2020-07-19 22:18:46 +0300

    Check Box, Scroll Pane and List

--- GUI/TextArea1.java
+++ GUI/TextArea1.java
@@ -1,12 +1,18 @@
 package GUI;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
-public class TextArea1 implements ActionListener {
+public class TextArea1 implements ActionListener, ItemListener, ListSelectionListener {
     JTextArea text;
+    JCheckBox checkBox;
+    JList list;
 
     public static void main(String[] args) {
         TextArea1 gui = new TextArea1();
@@ -18,20 +24,36 @@ public class TextArea1 implements ActionListener {
         JPanel panel = new JPanel();
         JButton button = new JButton("click it");
 
+        String listEntries[] = {"alpha", "beta", "gamma", "delta", "epsilon", "zeta", "eta", "theta"};
+        list = new JList<>(listEntries);
+        list.setVisibleRowCount(4);
+        list.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+        list.addListSelectionListener(this);
+
         button.addActionListener(this);
+
+        checkBox = new JCheckBox("Goes to 11");
+        checkBox.addItemListener(this);
+        checkBox.setSelected(true);
 
         text = new JTextArea(10, 20);
         text.setLineWrap(true);
-
         JScrollPane scrollPane = new JScrollPane(text);
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 
         panel.add(scrollPane);
 
+        JScrollPane scroller = new JScrollPane(list);
+        scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        scroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+        panel.add(scroller);
+
+        frame.getContentPane().add(BorderLayout.WEST, checkBox);
         frame.getContentPane().add(BorderLayout.CENTER, panel);
         frame.getContentPane().add(BorderLayout.SOUTH, button);
-        frame.setSize(350, 300);
+        frame.setSize(550, 500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
@@ -39,5 +61,26 @@ public class TextArea1 implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         text.append("button clicked \n ");
+    }
+
+    @Override
+    public void itemStateChanged(ItemEvent ev) {
+        String onOrOff = "off";
+
+        if (checkBox.isSelected()) {
+            onOrOff = "on";
+            System.out.println("Check box is " + onOrOff);
+        } else {
+            onOrOff = "off";
+            System.out.println("Check box is " + onOrOff);
+        }
+    }
+
+    @Override
+    public void valueChanged(ListSelectionEvent e) {
+        if (!e.getValueIsAdjusting()) {
+            String selection = (String) list.getSelectedValue();
+            System.out.println(selection);
+        }
     }
 }
