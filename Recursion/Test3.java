15fbed62071147c7c0ab3bb20f8a89e005755a94
Georgy <gogalsh@gmail.com>
2020-08-04 23:24:14 +0300

    Fibonachi example

---
+++ Recursion/Test3.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+public class Test3 {
+
+    static int fibonachi(int n) {
+
+        if (n == 0) {
+            return 0;
+        }
+
+        if (n == 1) {
+            return 1;
+        }
+        else{
+            return fibonachi(n - 1) + fibonachi(n - 2);
+        }
+    }
+
+}
