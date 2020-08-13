0aa8c9dbaa868c81eb66f2e81989c214a535d445
Georgy <gogalsh@gmail.com>
2020-08-13 22:28:45 +0300

    Added support of interface Comparable. Songs now compares with eachother by title

---
+++ GenericsAndCollections/Song.java
@@ -0,0 +1,41 @@
+package GenericsAndCollections;
+
+public class Song implements Comparable<Song> {
+    String title;
+    String artist;
+    String rating;
+    String bpm;
+
+    Song(String t, String a, String r, String b) {
+        title = t;
+        artist = a;
+        rating = r;
+        bpm = b;
+    }
+
+    @Override
+    public int compareTo(Song s) {
+        return title.compareTo(s.getTitle());
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public String getBpm() {
+        return bpm;
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
+}
