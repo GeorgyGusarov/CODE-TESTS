6b0ff433aa1b15081b45ba6201d1bfefd4159e73
Georgy <gogalsh@gmail.com>
2020-10-25 22:58:27 +0300

    Lesson 2 - Exceptions

--- Geekbrains_Java2/Lesson_2/Exception.java
+++ Geekbrains_Java2/Lesson_2/Exception.java
@@ -1,14 +1,16 @@
 package Geekbrains_Java2.Lesson_2;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class Exception {
     public static void main(String[] args) {
-
-        int a, b;
+        int b;
 
         try {
-            int[] res = {1, 2, 3};
+            int[] array = {1, 2, 3};
             b = 10 / 0;
-            res[10] = 20;
+            array[10] = 20;
         } catch (ArithmeticException e) {
             e.printStackTrace();
             System.out.println("Делить на ноль нельзя!");
@@ -16,8 +18,24 @@ public class Exception {
             ex.printStackTrace();
             System.out.println("Вышли за пределы массива!");
         } finally {
-            System.out.println("выполнится");
+            System.out.println("Выполнится в любом случае");
         }
         System.out.println("end");
+
+        try {
+            AlertException();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            FileInputStream fileInputStream = new FileInputStream("1.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void AlertException() throws FileNotFoundException {
+        FileInputStream fileInputStream = new FileInputStream("1.txt");
     }
 }
