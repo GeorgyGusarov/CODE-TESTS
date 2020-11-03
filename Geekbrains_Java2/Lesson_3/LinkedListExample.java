cd71cb516b82df4d4ae031d3fb9295043faa9752
Georgy <gogalsh@gmail.com>
2020-11-03 23:32:24 +0300

    Lesson 3 - Collections

---
+++ Geekbrains_Java2/Lesson_3/LinkedListExample.java
@@ -0,0 +1,36 @@
+package Geekbrains_Java2.Lesson_3;
+
+import java.util.*;
+
+public class LinkedListExample {
+    public static void main(String[] args) {
+        LinkedList<String> linkedList = new LinkedList<>();
+        linkedList.add("a");
+        linkedList.add("b");
+        linkedList.add("c");
+        linkedList.addFirst("z");
+
+        System.out.println(linkedList);
+    }
+}
+
+class BoxLinkedList {
+    String color;
+    int size;
+
+    public BoxLinkedList(String color, int size) {
+        this.color = color;
+        this.size = size;
+    }
+}
+
+class MainBoxLinkedList {
+    public static void main(String[] args) {
+        BoxLinkedList box1 = new BoxLinkedList("red", 1);
+        BoxLinkedList box2 = new BoxLinkedList("red", 1);
+
+        System.out.println(box1.hashCode());
+        System.out.println(box2.hashCode());
+        System.out.println(box1.equals(box2));
+    }
+}
