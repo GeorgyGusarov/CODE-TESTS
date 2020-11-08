da39843d172bf9e8bd3a8ae6114382355c8c127f
Georgy <gogalsh@gmail.com>
2020-11-08 23:43:52 +0300

    Lesson 4 - Graphical interface

---
+++ Geekbrains_Java2/Lesson_4/MyWindow2.java
@@ -0,0 +1,49 @@
+package Geekbrains_Java2.Lesson_4;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class MyWindow2 extends JFrame {
+    public MyWindow2() throws HeadlessException {
+        setTitle("Java Swing2");
+        setBounds(800, 300, 400, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+
+        JButton button1 = new JButton("button1");
+        JButton button2 = new JButton("button2");
+
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("button1");
+            }
+        });
+        button1.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                System.out.println(e.getX() + " " + e.getY());
+            }
+        });
+
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("button2");
+            }
+        });
+
+        JPanel panel = new JPanel(new GridLayout(1, 2));
+        panel.add(button1);
+        panel.add(button2);
+
+        add(panel, BorderLayout.SOUTH);
+    }
+}
+
+class MyWindow2Test {
+    public static void main(String[] args) {
+        new MyWindow2();
+    }
+}
---
+++ Geekbrains_Java2/Lesson_4/MyWindow3.java
@@ -0,0 +1,56 @@
+package Geekbrains_Java2.Lesson_4;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyWindow3 extends JFrame {
+    public MyWindow3() {
+        setTitle("Online Chat");
+        setBounds(800,300,400,400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel bottomPanel = new JPanel();
+        JPanel centerPanel = new JPanel();
+        JTextArea textArea = new JTextArea();
+        JTextField textField = new JTextField();
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        JButton button = new JButton("Send");
+
+        bottomPanel.setBackground(Color.gray);
+        centerPanel.setBackground(Color.lightGray);
+
+        bottomPanel.setPreferredSize(new Dimension(1, 40));
+
+        add(bottomPanel, BorderLayout.SOUTH);
+        add(centerPanel, BorderLayout.CENTER);
+
+        bottomPanel.setLayout(new FlowLayout());
+        centerPanel.setLayout(new BorderLayout());
+
+        centerPanel.add(scrollPane, BorderLayout.CENTER);
+
+        bottomPanel.add(textField);
+        bottomPanel.add(button);
+        textField.setPreferredSize(new Dimension(300, 28));
+        textArea.setEditable(false);
+
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textArea.append(textField.getText() + "\n");
+                textField.setText(" ");
+                textField.grabFocus();
+            }
+        });
+
+        setVisible(true);
+    }
+}
+
+class MyWindow3Test {
+    public static void main(String[] args) {
+        new MyWindow3();
+    }
+}
