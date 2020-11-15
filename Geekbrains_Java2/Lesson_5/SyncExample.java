9ae7f2631467b96dee0bb01a255ba49e91fe0d00
Georgy <gogalsh@gmail.com>
2020-11-15 19:34:17 +0300

    Lesson 5 - Multithreading

---
+++ Geekbrains_Java2/Lesson_5/SyncExample.java
@@ -0,0 +1,63 @@
+package Geekbrains_Java2.Lesson_5;
+
+public class SyncExample {
+    Object object = new Object();
+
+    void doSomething() {
+        /**
+         * параллельно
+         */
+        System.out.println(1);
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        /**
+         * этот кусок кода выполняется последовательно, остальные параллельно
+         */
+        synchronized (object) {
+            System.out.println(201);
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(202);
+        }
+        /**
+         * параллельно
+         */
+        System.out.println(2);
+    }
+}
+
+class SyncExampleMain {
+    public static void main(String[] args) {
+        SyncExample syncExample = new SyncExample();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                syncExample.doSomething();
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                syncExample.doSomething();
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                syncExample.doSomething();
+            }
+        }).start();
+    }
+}
