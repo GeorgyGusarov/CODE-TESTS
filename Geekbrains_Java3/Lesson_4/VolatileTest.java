3e5c3386192092cd59f18a69e97800e1581b0d42
Georgy <gogalsh@gmail.com>
2021-01-08 20:24:51 +0300

    different examples of multithreading

---
+++ Geekbrains_Java3/Lesson_4/VolatileTest.java
@@ -0,0 +1,39 @@
+package Geekbrains_Java3.Lesson_4;
+
+public class VolatileTest {
+    private static volatile int MY_INT = 0;
+
+    public static void main(String[] args) {
+        new ChangeListener().start();
+        new ChangeMarker().start();
+    }
+
+    static class ChangeListener extends Thread {
+        @Override
+        public void run() {
+            int local_value = MY_INT;
+            while (local_value < 5) {
+                if (local_value != MY_INT) {
+                    System.out.println("Got change for MY_INT " + MY_INT);
+                    local_value = MY_INT;
+                }
+            }
+        }
+    }
+
+    static class ChangeMarker extends Thread {
+        @Override
+        public void run() {
+            int local_value = MY_INT;
+            while (MY_INT < 5) {
+                System.out.println("Incrementing MY_INT " + local_value);
+                MY_INT = ++local_value;
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
