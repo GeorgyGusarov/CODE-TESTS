d15cf9d20c782f38ff1b87e01334fdf4c4899635
Georgy <gogalsh@gmail.com>
2021-01-12 22:54:50 +0300

    simple example of MFU solution

---
+++ Geekbrains_Java3/Lesson_4/MFU.java
@@ -0,0 +1,61 @@
+package Geekbrains_Java3.Lesson_4;
+
+public class MFU {
+    public static void main(String[] args) {
+        final MFU mfu = new MFU();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                mfu.print("Doc 1", 10);
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                mfu.print("Doc 2", 5);
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                mfu.scan("Doc 3", 5);
+            }
+        }).start();
+    }
+
+    Object printLock = new Object();
+    Object scanLock = new Object();
+
+    public void print(String doc, int n) {
+        synchronized (printLock) {
+            System.out.println("Начало печати");
+//            for (int i = 0; i < 10; i++) {
+//                System.out.print(i);
+//            }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Конец печати");
+        }
+    }
+
+    public void scan(String doc, int n) {
+        synchronized (scanLock) {
+            System.out.println("Начало сканирования");
+//            for (int i = 0; i < 10; i++) {
+//                System.out.print(i);
+//            }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Конец сканирования");
+        }
+    }
+}
