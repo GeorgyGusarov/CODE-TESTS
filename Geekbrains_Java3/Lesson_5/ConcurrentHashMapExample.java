b4bb8f63c9d3e0d42cd8c76b0069c7b530554a32
Georgy <gogalsh@gmail.com>
2021-01-18 23:08:06 +0300

    more of multithreading

---
+++ Geekbrains_Java3/Lesson_5/ConcurrentHashMapExample.java
@@ -0,0 +1,10 @@
+package Geekbrains_Java3.Lesson_5;
+
+import java.util.*;
+import java.util.concurrent.*;
+
+public class ConcurrentHashMapExample {
+    public static void main(String[] args) {
+        ConcurrentHashMap<String, String> chm = new ConcurrentHashMap<>();
+    }
+}
---
+++ Geekbrains_Java3/Lesson_5/SynchronizedQueue.java
@@ -0,0 +1,31 @@
+package Geekbrains_Java3.Lesson_5;
+
+import java.util.concurrent.*;
+
+public class SynchronizedQueue {
+    public static void main(String[] args) {
+        ArrayBlockingQueue<String> abq = new ArrayBlockingQueue<>(3);
+        abq.add("abc");
+        abq.add("abc");
+        abq.add("abc");
+
+        Boolean result1 = abq.offer("abc");
+        try {
+            Boolean result2 = abq.offer("abc", 100, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            abq.put("abc");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            abq.take();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
