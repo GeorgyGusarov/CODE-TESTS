99e330f4fca95f5d8e725a8fee169c2b2e8e8faf
Georgy <gogalsh@gmail.com>
2021-01-27 23:19:16 +0300

    annotations & reflection API

---
+++ Geekbrains_Java3/Lesson_7/HomeworkCheck/XField.java
@@ -0,0 +1,9 @@
+package Geekbrains_Java3.Lesson_7.HomeworkCheck;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface XField {
+
+}
