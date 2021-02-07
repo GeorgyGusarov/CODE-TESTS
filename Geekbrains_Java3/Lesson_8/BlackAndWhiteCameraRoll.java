aed5e6a88acd10a0c470498f8d7ef81bd7a0accb
Georgy <gogalsh@gmail.com>
2021-02-07 22:40:29 +0300

    spring

---
+++ Geekbrains_Java3/Lesson_8/BlackAndWhiteCameraRoll.java
@@ -0,0 +1,8 @@
+package Geekbrains_Java3.Lesson_8;
+
+public class BlackAndWhiteCameraRoll implements ICameraRoll {
+    @Override
+    public void takeAPhoto() {
+        System.out.println("-1 ч/б кадр!");
+    }
+}
