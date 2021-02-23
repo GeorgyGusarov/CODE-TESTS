1a1436ce3b6e7b11b6fb0363c81ae874a40e7185
Georgy <gogalsh@gmail.com>
2021-02-23 18:03:38 +0300

    example of iterator for related list

---
+++ AlgorithmsAndDatastructures/DataStructures/MyIterator.java
@@ -0,0 +1,13 @@
+package AlgorithmsAndDatastructures.DataStructures;
+
+import java.util.Iterator;
+
+public interface MyIterator<T> extends Iterator {
+    void reset();
+    boolean atEnd();
+    boolean atBegin();
+    T deleteCurrent();
+    void insertAfter(T c);
+    void insertBefore(T c);
+    T getCurrent();
+}
