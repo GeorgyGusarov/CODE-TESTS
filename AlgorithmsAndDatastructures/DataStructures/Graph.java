af673ef37ffd5684a1a7ec713e0990efc4370364
Georgy <gogalsh@gmail.com>
2021-03-13 14:06:51 +0300

    example of graph

--- AlgorithmsAndDatastructures/DataStructures/Graph.java
+++ AlgorithmsAndDatastructures/DataStructures/Graph.java
@@ -1,5 +1,7 @@
 package AlgorithmsAndDatastructures.DataStructures;
 
+import java.util.Stack;
+
 public class Graph {
 
     // вершина графа (ака узел)
@@ -49,5 +51,56 @@ public class Graph {
     // вывод информации о запрашиваемой вершине
     public void displayVertex(int vertex) {
         System.out.println(vertexList[vertex]);
+    }
+
+    public int getUnvisitedVertex(int vertex) {
+        for (int i = 0; i < size; i++) {
+            if (adjMatrix[vertex][i] == 1 && !vertexList[i].wasVisited) return i;
+        }
+        return -1;
+    }
+
+    // обход в глубину
+    public void depthTraverse() {
+        StackTest stack = new StackTest(MAX_VERTICES);
+        vertexList[0].wasVisited = true;
+        displayVertex(0);
+        stack.push(0);
+
+        while (!stack.isEmpty()) {
+            int v = getUnvisitedVertex(stack.peek());
+            if (v == -1) {
+                stack.pop();
+            } else {
+                vertexList[v].wasVisited = true;
+                displayVertex(v);
+                stack.push(v);
+            }
+        }
+        resetFlags();
+    }
+
+    // обход в ширину
+    public void widthTravers() {
+        QueueTest queue = new QueueTest(MAX_VERTICES);
+        vertexList[0].wasVisited = true;
+        displayVertex(0);
+        queue.insert(0);
+
+        while (!queue.isEmpty()) {
+            int vCurrent = queue.remove();
+            displayVertex(vCurrent);
+            int vNext;
+            while ((vNext = getUnvisitedVertex(vCurrent)) != -1) {
+                vertexList[vNext].wasVisited = true;
+                queue.insert(vNext);
+            }
+        }
+    }
+
+    private void resetFlags() {
+        for (int i = 0; i < size; i++) {
+            vertexList[i].wasVisited = false;
+        }
     }
 }
--- AlgorithmsAndDatastructures/DataStructures/GraphMain.java
+++ AlgorithmsAndDatastructures/DataStructures/GraphMain.java
@@ -1,4 +1,17 @@
 package AlgorithmsAndDatastructures.DataStructures;
 
 public class GraphMain {
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+        graph.addVertex('a');
+        graph.addVertex('b');
+        graph.addVertex('c');
+        graph.addVertex('d');
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(1, 3);
+        graph.depthTraverse();
+        System.out.println();
+        graph.widthTravers();
+    }
 }
