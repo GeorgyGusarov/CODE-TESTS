f45aad61e29423dead903e9c4a67635bf48f752e
Georgy <gogalsh@gmail.com>
2021-03-29 21:53:46 +0300

    example of Graph

--- AlgorithmsAndDatastructures/DataStructures/Graph.java
+++ AlgorithmsAndDatastructures/DataStructures/Graph.java
@@ -1,5 +1,6 @@
 package AlgorithmsAndDatastructures.DataStructures;
 
+import java.io.IOException;
 import java.util.Stack;
 
 public class Graph {
@@ -7,30 +8,30 @@ public class Graph {
     // вершина графа (ака узел)
     private class Vertex {
         char label;
+        Vertex parent;
         boolean wasVisited;
 
         public Vertex(char label) {
             this.label = label;
-            this.wasVisited = false;
+            wasVisited = false;
         }
 
         @Override
         public String toString() {
-            return "Vertex{" +
-                    "label=" + label +
-                    '}';
+            return "V: " + label;
         }
     }
 
-    private final int MAX_VERTICES = 32; // максимальное количество вершин по умолчанию
+    private final int MAX_VERTICES; // максимальное количество вершин по умолчанию
     private Vertex[] vertexList; // лист вершин
     private int[][] adjMatrix; // матрица смежностей
     private int size; // текущий размер
 
-    public Graph() {
+    public Graph(int maxVertices) {
+        MAX_VERTICES = maxVertices;
         vertexList = new Vertex[MAX_VERTICES];
         adjMatrix = new int[MAX_VERTICES][MAX_VERTICES];
-        size = 0;
+        this.size = 0;
     }
 
     // добавить узел
@@ -44,13 +45,9 @@ public class Graph {
         adjMatrix[end][start] = 1;
     }
 
-    public void displayAdjMatrix() {
-
-    }
-
     // вывод информации о запрашиваемой вершине
     public void displayVertex(int vertex) {
-        System.out.println(vertexList[vertex]);
+        System.out.print(vertexList[vertex] + "-> ");
     }
 
     public int getUnvisitedVertex(int vertex) {
@@ -61,7 +58,7 @@ public class Graph {
     }
 
     // обход в глубину
-    public void depthTraverse() {
+    public void depthTraverse() throws IOException {
         StackTest stack = new StackTest(MAX_VERTICES);
         vertexList[0].wasVisited = true;
         displayVertex(0);
@@ -93,14 +90,95 @@ public class Graph {
             int vNext;
             while ((vNext = getUnvisitedVertex(vCurrent)) != -1) {
                 vertexList[vNext].wasVisited = true;
+                displayVertex(vNext);
                 queue.insert(vNext);
             }
         }
+        resetFlags();
+    }
+
+    public QueueTest widthTraversPath(char from, char to) {
+        int start = getIndex(from);
+        int stop = getIndex(to);
+
+        QueueTest queue = new QueueTest(MAX_VERTICES);
+        vertexList[start].wasVisited = true;
+        queue.insert(start);
+        boolean done = false;
+        while (!queue.isEmpty()) {
+            int v1 = queue.remove();
+            int v2;
+            while ((v2 = getUnvisitedVertex(v1)) != -1) {
+                vertexList[v2].wasVisited = true;
+
+                if (v2 == stop) {
+                    done = true;
+                    break;
+                }
+                queue.insert(v2);
+            }
+        }
+        resetFlags();
+        if (done)
+            return queue;
+        else
+            return null;
+    }
+
+    private int getIndex(char c) {
+        for (int i = 0; i < vertexList.length; i++) {
+            if (vertexList[i].label == c)
+                return i;
+        }
+        return -1;
     }
 
     private void resetFlags() {
         for (int i = 0; i < size; i++) {
             vertexList[i].wasVisited = false;
         }
+    }
+
+    StackTest shortWay(char from, char to) {            //граф не взвешен
+        StackTest result = new StackTest(MAX_VERTICES);
+        QueueTest queue = new QueueTest(MAX_VERTICES);
+
+        int start = getIndex(from);
+        int stop = getIndex(to);
+        if (start == -1 || stop == -1 || start == stop)
+            return null;
+
+        vertexList[start].wasVisited = true;
+        queue.insert(start);
+
+        while (!queue.isEmpty()) {                  // ищем узел, помечаем родителей
+            int vCur = queue.remove();
+            int vNxt;
+            while ((vNxt = getUnvisitedVertex(vCur)) != -1) {
+                vertexList[vNxt].parent = vertexList[vCur];
+                vertexList[vNxt].wasVisited = true;
+                if (vNxt == stop) break;
+                queue.insert(vNxt);
+            }
+            if (vNxt == stop) break;
+        }
+        if (!vertexList[stop].wasVisited) return null;
+
+        result.push(vertexList[stop].label);
+        int current = stop;
+
+        while (vertexList[current].parent != null)        // идём обратно к старту по родителям
+            for (int i = 0; i < vertexList.length; i++)
+                if(vertexList[current].parent == vertexList[i]) {
+                    result.push(vertexList[i].label);
+                    current = i;
+                    break;
+                }
+
+        for (int i = 0; i < size; i++) {
+            vertexList[i].wasVisited = false;
+            vertexList[i].parent = null;
+        }
+        return result;
     }
 }
--- AlgorithmsAndDatastructures/DataStructures/GraphMain.java
+++ AlgorithmsAndDatastructures/DataStructures/GraphMain.java
@@ -1,17 +1,55 @@
 package AlgorithmsAndDatastructures.DataStructures;
 
+import java.io.IOException;
+import java.util.Arrays;
+
 public class GraphMain {
     public static void main(String[] args) {
-        Graph graph = new Graph();
-        graph.addVertex('a');
-        graph.addVertex('b');
-        graph.addVertex('c');
-        graph.addVertex('d');
-        graph.addEdge(0, 1);
-        graph.addEdge(0, 2);
-        graph.addEdge(1, 3);
-        graph.depthTraverse();
-        System.out.println();
-        graph.widthTravers();
+        Graph g = new Graph(32);
+        g.addVertex('a');
+        g.addVertex('b');
+        g.addVertex('c');
+        g.addVertex('d');
+        g.addVertex('e');
+        g.addVertex('f');
+        g.addVertex('g');
+        g.addVertex('h');
+        g.addVertex('i');
+        g.addVertex('j');
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 3);
+        g.addEdge(1, 4);
+        g.addEdge(3, 0);
+        g.addEdge(2, 5);
+        g.addEdge(5, 6);
+        g.addEdge(3, 7);
+        g.addEdge(6, 8);
+        g.addEdge(6, 9);
+        g.addEdge(7, 9);
+
+        try {
+            g.depthTraverse();
+            System.out.println();
+            System.out.println("--------------");
+            g.widthTravers();
+            System.out.println();
+            System.out.println("--------------");
+
+            StackTest shortWay = g.shortWay('a', 'i');
+            if (shortWay != null)
+                while (!shortWay.isEmpty()) {
+                    System.out.print((char) shortWay.pop());
+                    System.out.print((shortWay.isEmpty() ? "" : " -> "));
+                }
+
+            // why can't we simply print a queue when we got to final vertex
+            System.out.println();
+            QueueTest q = g.widthTraversPath('a', 'i');
+            System.out.println(Arrays.toString(q.getQueue()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
+
