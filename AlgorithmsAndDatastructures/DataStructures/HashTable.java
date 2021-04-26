0e014f1394ec108fc0e406c618f8ab3fa4ea1374
Georgy <gogalsh@gmail.com>
2021-04-26 22:35:07 +0300

    added linearProbe(), insert(), delete(), find(). IncreaseCapacity and isFull() throws NPE

--- AlgorithmsAndDatastructures/DataStructures/HashTable.java
+++ AlgorithmsAndDatastructures/DataStructures/HashTable.java
@@ -3,7 +3,7 @@ package AlgorithmsAndDatastructures.DataStructures;
 import java.util.Objects;
 
 public class HashTable {
-    private static class Item {
+    public static class Item {
         int data;
 
         public Item(int data) {
@@ -30,10 +30,12 @@ public class HashTable {
 
     private Item[] hashArray;
     private int arrSize;
+    private Item nullItem;
 
     public HashTable(int capacity) {
         this.hashArray = new Item[capacity];
         this.arrSize = capacity;
+        this.nullItem = new Item(-1);
     }
 
     @Override
@@ -52,6 +54,14 @@ public class HashTable {
         return key % arrSize;
     }
 
+    /**
+     * линейное пробирование
+     */
+    private int linearProbe(int hashVal) {
+        ++hashVal; // увеличиваем хэш
+        hashVal %= arrSize; // при необходимости перекидываем в начало массива
+        return hashVal; // и возвращаем
+    }
 
     /**
      * поиск элемента
@@ -62,9 +72,56 @@ public class HashTable {
             if (hashArray[hashVal].getData() == key) { // содержит ли она искомый ключ
                 return hashArray[hashVal]; // если да, то возвращаем значение
             }
-            ++hashVal; // если не нашли, перемещаемся на следующее значение
+            hashVal = linearProbe(hashVal);
+        }
+        return null;
+    }
+
+    /**
+     * вставка элимента
+     */
+    public void insert(Item item) {
+        int key = item.getData();
+        int hashVal = hashFunc(key);
+//        if (isFull()) increaseCapacity();
+        while (hashArray[hashVal] != null && hashArray[hashVal] != nullItem) {
+            hashVal = linearProbe(hashVal);
+        }
+        hashArray[hashVal] = item;
+    }
+
+    /**
+     * удаляет ключ по значению
+     */
+    public Item delete(int key) {
+        int hashVal = hashFunc(key);
+        while (hashArray[hashVal] != null) {
+            if (hashArray[hashVal].getData() == key) {
+                Item temp = hashArray[hashVal];
+                hashArray[hashVal] = nullItem;
+                return temp;
+            }
+            ++hashVal;
             hashVal %= arrSize;
         }
         return null;
     }
+
+//    private boolean isFull() {
+//        for (int i = 0; i < hashArray.length; i++) {
+//            if (hashArray[i] == null || hashArray[i] == nullItem) {
+//                return true;
+//            }
+//        }
+//        return true;
+//    }
+//
+//    private void increaseCapacity() {
+//        arrSize *= 2;
+//        Item[] oldArr = hashArray;
+//        hashArray = new Item[arrSize];
+//        for (int i = 0; i < oldArr.length; i++) {
+//            insert(oldArr[i]);
+//        }
+//    }
 }
--- AlgorithmsAndDatastructures/DataStructures/HashTableMain.java
+++ AlgorithmsAndDatastructures/DataStructures/HashTableMain.java
@@ -2,6 +2,18 @@ package AlgorithmsAndDatastructures.DataStructures;
 
 public class HashTableMain {
     public static void main(String[] args) {
+        HashTable hashTable = new HashTable(10); // хэш 25
+        hashTable.insert(new HashTable.Item(10)); // 10 легло на 10 место
+        hashTable.insert(new HashTable.Item(20)); // 20 на 20 место (20 % 25 = 20)
+        hashTable.insert(new HashTable.Item(30)); // 30 на 5 место
+        hashTable.insert(new HashTable.Item(75)); // 75 коллизия с 50 (75 % 25 = 0)
+        hashTable.insert(new HashTable.Item(40)); // 40 легло на 15 место
+        hashTable.insert(new HashTable.Item(50)); // 50 коллизия с 75 и легло следом
+        hashTable.insert(new HashTable.Item(60)); // 60 коллизия с 10
+        hashTable.insert(new HashTable.Item(70)); // 70 коллизия с 20
+        System.out.println(hashTable.toString());
 
+        hashTable.delete(75); // удалили элемент и заменили на -1
+        System.out.println(hashTable.toString());
     }
 }
